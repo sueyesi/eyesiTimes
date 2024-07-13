2ea7fd73e9047a69383427cd8ee174a0791fdba5
sueyesi <publisher724@gmail.com>
2024-07-14 02:26:14 +0900

    fourth commit

--- main.js
+++ main.js
@@ -7,19 +7,30 @@ const searchInput = document.getElementById("searchInput");
 menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsCategory(event)));
 let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
 //let url = new URL( `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`); 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
 
 //리펙토링
 const getNews = async() =>{
     try{
-        const response = await fetch(url); // await  fetch함수가 완료될 때까지 기다려준다    
-        console.log("rrr", response);    
+        //url호출 전에 페이지 쿼리 추가
+        url.searchParams.set("page", page);// => &page=page         
+        url.searchParams.set("pageSize", pageSize); 
+
+        const response = await fetch(url); // await  fetch함수가 완료될 때까지 기다려준다
+        //console.log("rrr", response);    
         const data = await response.json();// json파일형식
         if(response.status===200){
             if(data.articles.length === 0){
                 throw new Error("No result for this search")
             } 
             newsList = data.articles;
-            render();
+            totalResults = data.totalResults;
+            render(); 
+            paginationRender();
+           
         }else{
             throw new Error(data.message);
         }       
@@ -106,8 +117,71 @@ let searchOpen = () =>{
     document.getElementById("searchArea").style.width = "80%";
     document.getElementById("searchArea").style.opacity = "1";
 }
+
+//페이지네이션
+const paginationRender= ()=>{
+    //totalResult 
+    //page
+    //pageSizeg    
+    //groupSize
+    //totalPages
+    const totalPages = Math.ceil(totalResults / pageSize);//올림
+    //pageGroup
+    const pageGroup = Math.ceil(page / groupSize);//올림
+    //lastPage
+    let lastPage = pageGroup * groupSize;
+    //마지막 그룹이 그룹사이즈보다 작을경우 lastpage = totalpage
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+    //firstPage
+    const firstPage = lastPage - (groupSize - 1) <= 0? 1 : lastPage - (groupSize - 1);
+    let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>`;
+    
+    for(let i=firstPage;i<=lastPage;i++){
+        paginationHTML += `<li class="page-item ${i===page?'active':''}" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`
+    }
+    paginationHTML+=`<li class="page-item" onclick="moveToPage(${page+1})">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>`
+    document.querySelector(".pagination").innerHTML = paginationHTML;  
+};
+//page클릭시 URL호출
+const moveToPage = async (pageNum) => {
+    //console.log("m",pageNum);
+    page = pageNum;
+    await getNews();
+}
+
 getLatestNews();
 // 삼항조건연산자 A ? B : C (A는 참이면 B 거짓이면 C를 실행)
 ////news.description.length > 0 조건을 사용하여 description이 비어있지 않은 경우에는 말줄임 표시, 비어있는 경우에는 내용없음 표시
 //URL 관련문서 https://developer.mozilla.org/en-US/docs/Web/API/URL
 //await  비동기 함수 내에서 사용되며, 해당 함수가 완료될 때까지 기다리도록 하는 역할을 한다.
+/* pagination
+
+
+
+
+pageGrop(내가속한페이지그룹) = 내가 현재 보고있는 페이지 / 그룹사이즈
+
+- totalResults = 주어지는 값
+- pageSize = 정하는값
+- page = 정하는값
+- groupSize = 정하는값(몇개씩 보여줄건지)
+구해야하는값
+- totalPage = (totalResults / pageSize) 반올림해준다. math.ceil()
+math.ceil() 함수는 주로 소수점 이하를 올림하여 다음 정수로 반올림하는 경우에 사용
+- pageGroup = (page/groupSize) math.ceil() 반올림
+
+마지막페이지값 = pageGroup * groupSize
+첫번째페이지 = 마지막 - (groupSize-1)
+
+
+*/
